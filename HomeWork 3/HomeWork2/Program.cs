b25b5ffaa269b4023ab3520cc279f46e87014555
VitalikVerbytskii <134941871+VitalikVerbytskii@users.noreply.github.com>
2023-06-07 21:40:49 +0300

    Update Program.cs

--- HomeWork 3/HomeWork2/Program.cs
+++ HomeWork 3/HomeWork2/Program.cs
@@ -1,15 +1,15 @@
 ﻿int Number = 69845;
 
-int firstNumber = Number / 10000;//6
-int secondNumber = Number / 1000 - firstNumber * 10;//9
-int thirdNumber = Number / 100 - (firstNumber * 100 + secondNumber * 10);//8
-int fourthNumber = Number / 10 - (firstNumber * 1000 + secondNumber * 100 + thirdNumber * 10);//4
-int fifthNumber = Number - (firstNumber * 10000 + secondNumber * 1000 + thirdNumber * 100 + fourthNumber * 10);//5
+int FirstNumber = Number / 10000;
+int SecondNumber = Number / 1000 - FirstNumber * 10;
+int ThirdNumber = Number / 100 - (FirstNumber * 100 + SecondNumber * 10);
+int FourthNumber = Number / 10 - (FirstNumber * 1000 + SecondNumber * 100 + ThirdNumber * 10);
+int FifthNumber = Number - (FirstNumber * 10000 + SecondNumber * 1000 + ThirdNumber * 100 + FourthNumber * 10);
 
-Console.WriteLine(firstNumber);
-Console.WriteLine(secondNumber);
-Console.WriteLine(thirdNumber);
-Console.WriteLine(fourthNumber);
-Console.WriteLine(fifthNumber);
-//Console.WriteLine(firstNumber + " " + secondNumber + " " + thirdNumber + " " + fourthNumber + " " + fifthNumber);
+Console.WriteLine(FirstNumber);
+Console.WriteLine(SecondNumber);
+Console.WriteLine(ThirdNumber);
+Console.WriteLine(FourthNumber);
+Console.WriteLine(FifthNumber);
+//Console.WriteLine(FirstNumber + " " + SecondNumber + " " + ThirdNumber + " " + FourthNumber + " " + FifthNumber);
 Console.ReadLine();
