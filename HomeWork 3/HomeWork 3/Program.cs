c96ae9ca0083e4131d856cd9c553504f33cc118f
VitalikVerbytskii <vitaliyverb9@gmail.com>
2023-06-13 21:03:04 +0300

    HomeWork4

---
+++ HomeWork 3/HomeWork 3/Program.cs
@@ -0,0 +1,15 @@
+﻿int Number = 69845;
+
+int firstNumber = Number / 10000;//6
+int secondNumber = Number / 1000 - firstNumber * 10;//9
+int thirdNumber = Number / 100 - (firstNumber * 100 + secondNumber * 10);//8
+int fourthNumber = Number / 10 - (firstNumber * 1000 + secondNumber * 100 + thirdNumber * 10);//4
+int fifthNumber = Number - (firstNumber * 10000 + secondNumber * 1000 + thirdNumber * 100 + fourthNumber * 10);//5
+
+Console.WriteLine(firstNumber);
+Console.WriteLine(secondNumber);
+Console.WriteLine(thirdNumber);
+Console.WriteLine(fourthNumber);
+Console.WriteLine(fifthNumber);
+//Console.WriteLine(firstNumber + " " + secondNumber + " " + thirdNumber + " " + fourthNumber + " " + fifthNumber);
+Console.ReadLine();
---
+++ HomeWork4/HomeWork4/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Pleas write number...");
+int number = int.Parse(Console.ReadLine());
+
+string notprimenumber = (number + " is NOT a prime number");
+string primenumber = (number + " is a prime number");
+
+if (number < 2)
+{
+    Console.WriteLine(notprimenumber);
+}
+else if (number == 2)
+{
+    Console.WriteLine(primenumber);
+}
+else if (number % 2 == 0)
+{
+    Console.WriteLine(notprimenumber);
+
+}
+else
+{
+    int divisor = 3;
+    bool Prime = true;
+    while (divisor < number)
+    {
+        if (number % divisor == 0)
+        {
+            Prime = false;
+            Console.WriteLine(notprimenumber);
+            break;
+        }
+        divisor += 2;
+    }
+    if (Prime)
+    {
+        Console.WriteLine(primenumber);
+    }
+}
