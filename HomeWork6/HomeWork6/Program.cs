7e016b50aafa3b61fc6be5d456b9f8fba1873def
VitalikVerbytskii <vitaliyverb9@gmail.com>
2023-06-22 15:51:12 +0300

    HomeWork6

---
+++ HomeWork6/HomeWork6/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Please write index Number...");
+
+ int indexNumber = int.Parse(Console.ReadLine()); 
+
+int firstNumber = 1; 
+
+int i = 0;
+
+while (indexNumber > i)
+{
+   
+
+     if (indexNumber == 2)
+    {
+        //primeNumber
+        Console.WriteLine(firstNumber);
+    }
+    else if (firstNumber % 2 == 0)
+    {
+      firstNumber++;
+    }
+    else
+    {
+        int divisor = 3;
+
+        bool Prime = true;
+
+        while (divisor < firstNumber)
+        {
+            if (firstNumber % divisor == 0)
+            {
+                Prime = false;
+                firstNumber++;
+                //notPrimeNumber
+                break;
+            }
+            divisor += 2;
+        }
+        if (Prime)
+        {
+            firstNumber++;
+            i++;
+            //primeNumber
+        }
+        
+    }
+   if (indexNumber == i)
+    {
+  firstNumber--;
+  Console.WriteLine(firstNumber);
+    }
+}
+
+Console.ReadLine();
+
