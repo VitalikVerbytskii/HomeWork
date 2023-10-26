5710595d4f83168fcb5f3c8f8902e3dc53c4f396
VitalikVerbytskii <134941871+VitalikVerbytskii@users.noreply.github.com>
2023-10-26 21:56:57 +0300

    TaskManager

--- CourseWork/CourseWork/Program.cs
+++ CourseWork/CourseWork/Program.cs
@@ -1,15 +1,20 @@
-﻿class Program
-{
-    static List<TaskItem> taskList = new List<TaskItem>(); // Замість List<string> використовуємо список об'єктів TaskItem
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-    static string dataFilePath = "tasks.txt"; // Файл для збереження завдань
+class Program
+{
+    private static readonly List<TaskItem> taskList = new List<TaskItem>();
+    private static readonly string dataFilePath = "tasks.txt";
 
     static void Main(string[] args)
     {
-        LoadTasks(); // Завантаження завдань з файлу, якщо вони є
+        LoadTasks(); // Download tasks from the file, if there are any
 
         while (true)
         {
+            Console.Clear();
             Console.WriteLine("ToDo List");
             Console.WriteLine("1. Show tasks");
             Console.WriteLine("2. Add a task");
@@ -17,7 +22,7 @@
             Console.WriteLine("4. Delete task");
             Console.WriteLine("5. Task search");
             Console.WriteLine("6. Delete all tasks");
-            Console.WriteLine("0. Entrance");
+            Console.WriteLine("0. Exit");
             Console.Write("Select an option: ");
 
             if (int.TryParse(Console.ReadLine(), out int choice))
@@ -25,25 +30,25 @@
                 switch (choice)
                 {
                     case 1:
-                        ShowTasks();
+                        ShowTasks(); // Show a list of tasks
                         break;
                     case 2:
-                        AddTask();
+                        AddTask(); // Add a new task
                         break;
                     case 3:
-                        EditTask();
+                        EditTask(); // Editing an existing task
                         break;
                     case 4:
-                        DeleteTask();
+                        DeleteTask(); // Delete the task
                         break;
                     case 5:
-                        SearchTask();
+                        SearchTask(); // Looking for a task by name
                         break;
                     case 6:
-                        DeleteAllTasks();
+                        DeleteAllTasks(); // Delete all tasks
                         break;
                     case 0:
-                        SaveTasks(); // Збереження завдань в файл перед виходом
+                        SaveTasks(); // Save the task to a file before exiting
                         Environment.Exit(0);
                         break;
                     default:
@@ -58,9 +63,10 @@
         }
     }
 
-    // Відображення всіх завдань
+    // Shows a list of all tasks
     static void ShowTasks()
     {
+        Console.Clear();
         if (taskList.Any())
         {
             Console.WriteLine("List of tasks:");
@@ -75,9 +81,11 @@
             Console.WriteLine("There are no tasks.");
         }
         Console.WriteLine();
+        Console.WriteLine("Press Enter to continue...");
+        Console.ReadLine();
     }
 
-    // Додавання нового завдання
+    // Adds a new task
     static void AddTask()
     {
         Console.Write("Enter a task name: ");
@@ -85,9 +93,11 @@
         TaskItem newTask = new TaskItem(title);
         taskList.Add(newTask);
         Console.WriteLine("Task added.");
+        Console.WriteLine("Press Enter to continue...");
+        Console.ReadLine();
     }
 
-    // Редагування існуючого завдання
+    // Edits an existing task
     static void EditTask()
     {
         ShowTasks();
@@ -103,9 +113,11 @@
         {
             Console.WriteLine("Invalid input.");
         }
+        Console.WriteLine("Press Enter to continue...");
+        Console.ReadLine();
     }
 
-    // Видалення існуючого завдання
+    // Deletes an existing task
     static void DeleteTask()
     {
         ShowTasks();
@@ -119,9 +131,11 @@
         {
             Console.WriteLine("Invalid input.");
         }
+        Console.WriteLine("Press Enter to continue...");
+        Console.ReadLine();
     }
 
-    // Пошук завдання за назвою
+    // Searches for tasks by name
     static void SearchTask()
     {
         Console.Write("Enter the name of the task to search for: ");
@@ -132,6 +146,7 @@
             .Where(task => task.Task.Title.Contains(searchTitle, StringComparison.OrdinalIgnoreCase))
             .ToList();
 
+        Console.Clear();
         if (foundTasks.Any())
         {
             Console.WriteLine("Tasks found:");
@@ -145,9 +160,11 @@
         {
             Console.WriteLine("No task found.");
         }
+        Console.WriteLine("Press Enter to continue...");
+        Console.ReadLine();
     }
 
-    // Видалення всіх завдань
+    // Deletes all tasks
     static void DeleteAllTasks()
     {
         Console.Write("Are you sure you want to delete all tasks? (Yes/No): ");
@@ -157,9 +174,11 @@
             taskList.Clear();
             Console.WriteLine("All tasks have been deleted.");
         }
+        Console.WriteLine("Press Enter to continue...");
+        Console.ReadLine();
     }
 
-    // Збереження завдань в файл
+    // Saves the task to a file
     static void SaveTasks()
     {
         using (StreamWriter writer = new StreamWriter(dataFilePath))
@@ -171,7 +190,7 @@
         }
     }
 
-    // Завантаження завдань з файлу
+    // Loads a task from a file
     static void LoadTasks()
     {
         if (File.Exists(dataFilePath))
@@ -200,13 +219,13 @@ class TaskItem
         history = new List<string>();
         history.Add($"Created: {DateTime.Now}");
     }
-
+    // Edits the task name and adds a record of the change to the history.
     public void Edit(string newTitle)
     {
         history.Add($"Changed: {DateTime.Now}, New name: {newTitle}");
         Title = newTitle;
     }
-
+    // Outputs the history of changes in the task to the console.
     public void ShowHistory()
     {
         Console.WriteLine("History of changes:");
